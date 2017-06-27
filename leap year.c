36c56952f1797f796ed394de2cc05a82b6a576da
rajavikky <vikky3088@gmail.com>
2017-06-27 12:30:15 +0530

    Update leap year.c

--- leap year.c
+++ leap year.c
@@ -3,16 +3,16 @@ int main()
 {
 int a;
     scanf("%d",&a);
-    if((a%4==0)&&(a%400==0))
+    if(a%4==0)
     {
-        if(a%100==0)
+        if((a%100!=0)||(a%400==0))
         {
-            printf("not leap year");
+            printf("leap year");
         }
     }
     else
     {
-        printf("leap year");
+        printf("not leap year");
     }
 return 0;
 }
