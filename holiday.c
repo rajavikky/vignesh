a157b60ab1bb4f353f87486dd615f5e1ad9e88db
rajavikky <vikky3088@gmail.com>
2017-06-29 12:45:35 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{
+char i,
+str[10];
+clrscr();
+printf("day\n");
+//for(i=0;i<10;i++)
+
+scanf("%s",str);
+printf("%s",str);
+//for(i=0;i<10;i++)
+
+if(strcmp(str,"monday")==0)||strcmp(str,"tuesday"==0)||(strcmp(str,"wednesday")==0)||(strcmp(str,"thursday")==0)||(strcmp(str,"friday")==0)
+{
+printf("true");
+}
+else
+{
+printf("false");
+}
+
+getch();
+}
